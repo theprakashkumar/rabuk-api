7bfed01a5679f06342989d6faffcfadc5dba13a5
theprakashkumar <maiprakashkumar@gmail.com>
2025-04-18 21:49:12 +0530

    feat: Added logic to connect to DB.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import { connectDB } from "./config/database";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,12 +7,16 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-// Routes
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Welcome to Rabuk API" });
-});
+const startServer = async () => {
+  try {
+    // Connect to DB and then start the server.
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`⚡️Server is running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error);
+  }
+};
 
-// Start server
-app.listen(port, () => {
-  console.log(`⚡️Server is running at http://localhost:${port}`);
-});
+startServer();
