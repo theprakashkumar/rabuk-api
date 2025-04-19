b7f0fc3d071f747aeaf05fcaba25139c59fabb1f
theprakashkumar <maiprakashkumar@gmail.com>
2025-04-19 18:50:05 +0530

    feat: Added validator to project.

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,27 @@
+import { isStrongPassword } from "validator";
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  firstName: { type: String, required: true, minlength: 3, maxlength: 30 },
-  lastName: { type: String, required: true, minlength: 3, maxlength: 30 },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 8, maxlength: 1024 },
-  age: { type: Number, required: true, min: 18, max: 2300 },
-  gender: { type: String, required: true, enum: ["male", "female", "other"] },
-});
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true, minlength: 3, maxlength: 30 },
+    lastName: { type: String, required: true, minlength: 3, maxlength: 30 },
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: String,
+      required: true,
+      validate(value: string) {
+        if (!isStrongPassword(value)) {
+          throw new Error("Password is not strong");
+        }
+      },
+    },
+    age: { type: Number, required: true, min: 18, max: 2300 },
+    gender: { type: String, required: true, enum: ["male", "female", "other"] },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
 export const User = model("User", userSchema);
